92d6131f01effce8094321f5fbe339c561697737
jkfdo <obidev@proton.me>
2025-05-06 12:33:54 +0600

    data.js

--- data.js
+++ data.js
@@ -1,40 +1,96 @@
 const data = [
-  {
-    name: `Gravity`,
-    artist: `The Ambientalist`,
+      {
+        name: `Gravity`,
+        artist: `The Ambientalist`,
+        subArtist: ``,
+        genre: `Chillstep Chillout Ambient Electronic`,
+        mood: `Emotional Sorrow Sad Alone Dreamy`,
+        
+        channel: `AurVen`,
+        publishDate: `6 May 2025`,
+        
+        pbLnk: `https://youtu.be/p06TdtcBtbc`,
+        arLnk: `https://youtu.be/p06TdtcBtbc`,
+        bolume: `AurNov`,
+    
+        
+        artwork: ``,
+        art: `https://wallpapers.com/images/high/embracing-the-sublime-tranquility-a-glimpse-of-the-sunset-aesthetic-pii0lxq5jxqacmi5.webp`,
+      },
+      {
+      name: ``,
+      artist: ``,
+      subArtist: ``,
+      genre: ``,
+      mood: ``,
+      
+      channel: `AurVen`,
+      publishDate: `6 May 2025`,
+      
+      pbLnk: ``,
+      arLnk: ``,
+      bolume: `AurNov`,
+      
+      
+      artwork: ``,
+      art: ``,
+    },
+    {
+    name: ``,
+    artist: ``,
     subArtist: ``,
-    genre: `Chillstep Chillout Ambient Electronic`,
-    mood: `Emotional Sorrow Sad Alone Dreamy`,
+    genre: ``,
+    mood: ``,
     
     channel: `AurVen`,
     publishDate: `6 May 2025`,
     
-    pbLnk: `https://youtu.be/p06TdtcBtbc`,
-    arLnk: `https://youtu.be/p06TdtcBtbc`,
+    pbLnk: ``,
+    arLnk: ``,
     bolume: `AurNov`,
-
+    
     
     artwork: ``,
-    art: `https://wallpapers.com/images/high/embracing-the-sublime-tranquility-a-glimpse-of-the-sunset-aesthetic-pii0lxq5jxqacmi5.webp`,
+    art: ``,
   },
   {
-  name: `NObody`,
-  artist: `The Ambientalist`,
-  subArtist: ``,
-  genre: `Chillstep Chillout Ambient Electronic`,
-  mood: `Emotional Sorrow Sad Alone Dreamy`,
+      name: ``,
+      artist: ``,
+      subArtist: ``,
+      genre: ``,
+      mood: ``,
+      
+      channel: `AurVen`,
+      publishDate: `6 May 2025`,
+      
+      pbLnk: ``,
+      arLnk: ``,
+      bolume: `AurNov`,
+      
+      
+      artwork: ``,
+      art: ``,
+    },
+    {
+    name: ``,
+    artist: ``,
+    subArtist: ``,
+    genre: ``,
+    mood: ``,
+    
+    channel: `AurVen`,
+    publishDate: `6 May 2025`,
+    
+    pbLnk: ``,
+    arLnk: ``,
+    bolume: `AurNov`,
+    
+    
+    artwork: ``,
+    art: ``,
+  },
   
-  channel: `AurVen`,
-  publishDate: `6 May 2025`,
-  
-  pbLnk: `https://youtu.be/p06TdtcBtbc`,
-  arLnk: `https://youtu.be/p06TdtcBtbc`,
-  bolume: `AurNov`,
-  
-  
-  artwork: ``,
-  art: ``,
-},/*
+  /*
   {
     name: ``,
     artist: ``,
